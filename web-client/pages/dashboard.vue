02cf1aa338fe8b5dd99710d91bd55fafb8a76ff5
KardamSimeonov <masutayodagm@gmail.com>
2024-03-17 23:43:24 +0200

    Product and order cards in dashboard

--- web-client/pages/dashboard.vue
+++ web-client/pages/dashboard.vue
@@ -2,7 +2,7 @@
     <header class="lg:px-16 px-4 py-6">
         <nav class="flex justify-between">
             <span class="text-red-700 hover:text-red-500 cursor-pointer text-4xl font-bold"
-                style="font-family: calibri;">Food2U | Vendors</span>
+                style="font-family: calibri;">LocalFood | Vendors</span>
             <div class="flex items-center gap-3 cursor-pointer">
                 <span class="font-semibold text-xl">Logout</span>
                 <Icon name="material-symbols:login-rounded" class="text-3xl" />
@@ -15,6 +15,37 @@
             <aside
                 class="xl:col-span-4 col-span-full bg-red-500 xl:h-full h-[70vh] overflow-y-auto px-8 py-12 flex flex-col">
                 <h1 class="text-4xl text-white font-semibold mb-12">Your orders</h1>
+                <div class="bg-white shadow-lg rounded-lg p-6 mb-6">
+                    <div class="font-bold text-xl mb-4">Order #1</div>
+                        <div class="flex items-center mb-4">
+                            <img src="@/assets/cheese.jpg" alt="${product.ImageName}" class="w-16 h-16 object-cover rounded-lg mr-4">
+                            <div>
+                                <div class="font-bold">Product Title</div>
+                                <div class="text-gray-600">$3.50</div>
+                            </div>
+                        </div>
+                    <p class="font-semibold">Deliver to: Loading...</p>
+                    <button class="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 mt-4">Mark as sent</button>
+                </div>
+                <div class="bg-white shadow-lg rounded-lg p-6 mb-6">
+                    <div class="font-bold text-xl mb-4">Order #2</div>
+                        <div class="flex items-center mb-4">
+                            <img src="@/assets/cheese.jpg" alt="${product.ImageName}" class="w-16 h-16 object-cover rounded-lg mr-4">
+                            <div>
+                                <div class="font-bold">Product Title</div>
+                                <div class="text-gray-600">$3.50</div>
+                            </div>
+                        </div>
+                        <div class="flex items-center mb-4">
+                            <img src="@/assets/cheese.jpg" alt="${product.ImageName}" class="w-16 h-16 object-cover rounded-lg mr-4">
+                            <div>
+                                <div class="font-bold">Product Title</div>
+                                <div class="text-gray-600">$3.50</div>
+                            </div>
+                        </div>
+                    <p class="font-semibold">Deliver to: Loading...</p>
+                    <button class="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 mt-4">Mark as sent</button>
+                </div>
                 <div
                     class="border-dashed border-4 border-red-800 rounded-lg p-4 flex flex-col justify-center items-center min-h-[15rem]">
                     <p class="text-white text-center">You have no orders</p>
@@ -24,6 +55,12 @@
         <section class="py-12 text-white">
             <h1 class="text-4xl font-bold text-center mb-16 text-red-700">Your Products</h1>
             <div class="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 max-w-5xl px-4 mx-auto">
+                <div class="bg-red-500 rounded-lg p-4 flex flex-col justify-between relative">
+                    <img src="@/assets/cheese.jpg" alt="Product Image" class="w-full h-56 object-cover rounded-lg mb-6">
+                    <h2 class="text-xl font-bold">Product Title</h2>
+                    <p class="text-lg font-semibold mt-14 mb-4">$2.50</p>
+                    <button class="absolute bottom-4 right-4 bg-red-700 text-white px-4 py-2 rounded-lg hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50">Delete</button>
+                </div>
                 <div class="border-dashed border-4 border-red-500 hover:border-red-300 text-red-500 hover:text-red-300 cursor-pointer rounded-lg p-4 flex flex-col justify-center items-center min-h-[23rem]">
                     <Icon name="bi:plus-circle" class="text-6xl" />
                 </div>
