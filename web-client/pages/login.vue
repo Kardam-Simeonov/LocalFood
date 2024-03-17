d8954a6be7d78c15a418a0d9e66f0794889825d2
KardamSimeonov <masutayodagm@gmail.com>
2024-03-17 21:29:51 +0200

    Login page base layout

---
+++ web-client/pages/login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="min-h-screen bg-red-500 flex justify-center items-center">
+        <div class="bg-white rounded-lg p-8 max-w-md w-full">
+        <h1 class="text-red-800 text-2xl font-bold mb-4">LocalFood | Login</h1>
+        <div class="mb-4">
+            <label class="block text-gray-700 font-bold mb-2" for="email">
+            Email
+            </label>
+            <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Email">
+        </div>
+        <div class="mb-6">
+            <label class="block text-gray-700 font-bold mb-2" for="password">
+            Password
+            </label>
+            <input class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="Password">
+        </div>
+        <p class="text-red-500 text-sm mb-4">error field</p>
+        <div class="flex items-center justify-between">
+            <NuxtLink to="/dashboard">
+                <button class="bg-red-800 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                Login
+                </button>
+            </NuxtLink>
+        </div>
+        <div class="text-center mt-4">
+            <p>Don't have an account? <NuxtLink to="/register"><span class="underline hover:no-underline cursor-pointer font-semibold">Sign up</span></NuxtLink></p>
+        </div>
+        </div>
+    </div>
+</template>
