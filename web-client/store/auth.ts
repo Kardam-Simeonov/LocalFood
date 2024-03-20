e5cfaca2f5c3365eab880931473fb60108319d73
KardamSimeonov <masutayodagm@gmail.com>
2024-03-20 22:30:29 +0200

    Created an auth.ts store in pinia

---
+++ web-client/store/auth.ts
@@ -0,0 +1,37 @@
+import { defineStore } from 'pinia';
+
+interface UserPayloadInterface {
+  username: string;
+  password: string;
+}
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    authenticated: false,
+    loading: false,
+  }),
+  actions: {
+    async authenticateUser({ username, password }: UserPayloadInterface) {
+      const { data, pending }: any = await useFetch('https://dummyjson.com/auth/login', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: {
+          username,
+          password,
+        },
+      });
+      this.loading = pending;
+
+      if (data.value) {
+        const token = useCookie('token'); 
+        token.value = data?.value?.token; 
+        this.authenticated = true; 
+      }
+    },
+    logUserOut() {
+      const token = useCookie('token'); 
+      this.authenticated = false; 
+      token.value = null; 
+    },
+  },
+});
