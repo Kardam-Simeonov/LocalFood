2f0aa6d14b717f44710c18990be5059ec590a608
KardamSimeonov <masutayodagm@gmail.com>
2024-03-28 22:56:34 +0200

    Hooked up the Seed script in Program.cs

--- server/server/Program.cs
+++ server/server/Program.cs
@@ -13,6 +13,7 @@ namespace server
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddTransient<Seed>();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -22,6 +23,21 @@ namespace server
             });
 
             var app = builder.Build();
+
+            // Configure the setup for the seed script
+            if (args.Length == 1 && args[0].ToLower() == "seeddata")
+                SeedData(app);
+
+            void SeedData(IHost app)
+            {
+                var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+                using (var scope = scopedFactory.CreateScope())
+                {
+                    var service = scope.ServiceProvider.GetService<Seed>();
+                    service.SeedDataContext();
+                }
+            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
