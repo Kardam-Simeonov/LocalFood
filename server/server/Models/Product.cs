8486141fe203b20e89d1829ae248c95a1fad2a97
KardamSimeonov <masutayodagm@gmail.com>
2024-03-28 12:17:05 +0200

    Created a server ASP.NET Web MVC base project

---
+++ server/server/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace server.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public double Distance { get; set; }
+        public Seller Seller { get; set; }
+    }
+}
---
+++ server/server/Models/Seller.cs
@@ -0,0 +1,9 @@
+﻿namespace server.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
