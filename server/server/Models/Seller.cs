bf8ccf884971eca9d2094be8fc3e9a8bbadf0c4e
KardamSimeonov <masutayodagm@gmail.com>
2024-03-30 18:42:35 +0200

    Modified Seller Model

--- server/server/Models/Seller.cs
+++ server/server/Models/Seller.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public ICollection<Product> Products { get; set; }
