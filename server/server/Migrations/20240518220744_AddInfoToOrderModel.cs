3ef17fb03dfa3a09ecd70d73a0f845aba38705ce
KardamSimeonov <masutayodagm@gmail.com>
2024-05-19 01:08:29 +0300

    Added additional information properties to Order Model

---
+++ server/server/Migrations/20240518220744_AddInfoToOrderModel.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInfoToOrderModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Apartment",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryNote",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Entrance",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Floor",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ReadyForPickup",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Apartment",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DeliveryNote",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Entrance",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Floor",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ReadyForPickup",
+                table: "Orders");
+        }
+    }
+}
